3e33f1909a8fb6960d5fde339d336377b90d50b3
phillipschmidt-lutz <phillip.schmidtlutz@googlemail.com>
2021-01-30 17:37:00 +0100

    CRUDS

--- routes/route.js
+++ routes/route.js
@@ -2,8 +2,17 @@ const express = require('express')
 const router = express.Router();
 const Product = require('../Models/products')
 
-router.get('/', (req, res, next) => {
+router.get('/product', async (req, res, next) => {
 
+    const a = await Product.find()
+    res.json(a)
+})
+
+router.get('/product/:id', async (req, res, next) => {
+
+    const id = req.params.id
+    const a = await Product.findById(id)
+    res.json(a)
 })
 
 
@@ -24,5 +33,41 @@ router.post('/product', (req, res, next) => {
         })
 
 })
+
+
+router.put('/product/:id', (req, res) => {
+
+    const id = req.params.id
+    const updatedTitle = req.body.updatedTitle
+
+    Product.findById(id).then(product => {
+        product.title = updatedTitle
+        return product.save();
+    })
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    res.status(200,)
+
+})
+
+router.delete('/product/:id', (req, res) => {
+
+    const id = req.params.id
+
+    Product.findByIdAndRemove(id).then(product => {
+        console.log(product)
+    })
+        .catch(err => {
+            console.log(err)
+        })
+    res.status(200,)
+
+})
+
+
 
 module.exports = router
